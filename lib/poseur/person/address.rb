73a96e9d0ea19b18d4406891a1723ed42741176c
Carl Hicks <carl.hicks@gmail.com>
2012-10-04 21:55:08 -0700

    Strip trailing commas

--- lib/poseur/person/address.rb
+++ lib/poseur/person/address.rb
@@ -9,12 +9,13 @@ module Person
       @zip, @city, @state = random_line_from_postal_code_file
     end
     def random_line_from_postal_code_file
+      # Get the latest file from here: http://www.unitedstateszipcodes.org/zip_code_database.csv
       line = nil
       File.open("#{ROOT}/locales/postal_codes.usa") do |file|
         lines = file.readlines
         line  = lines[Random.rand(lines.size)]
       end
-      line.split(',').collect! { |x| x.strip }
+      line.split(',').collect! { |x| x.strip.gsub(/,/, '') }
     end
     def address
       @street + "\n" + @city + ", " + @state + " " + @zip
